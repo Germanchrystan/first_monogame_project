bf63334db9d56c6f454c6cfa2be4e8037cda4bf6
Germanchrystan <germanchrystan@gmail.com>
2023-10-24 00:15:46 -0300

    working game with sprites

--- 04_Jigsaw/Jigsaw/Piece.cs
+++ 04_Jigsaw/Jigsaw/Piece.cs
@@ -10,6 +10,8 @@ namespace Jigsaw
         private MouseState mState;
         public bool isSelected = false;
 
+        public static bool isMouseDragging = false;
+
         public Piece(int newPieceNumber, Vector2 newPosition, int newSize, Rectangle newRect)
         {
             tag = Globals.PIECE_TAG;
@@ -29,7 +31,7 @@ namespace Jigsaw
                
             if (mState.LeftButton == ButtonState.Pressed)
             {
-                if(!isSelected)
+                if(!isSelected && !isMouseDragging)
                 {
                     float mouseTargetDist = Vector2.Distance (
                         new Vector2(position.X + size.X / 2, position.Y + size.Y / 2),
@@ -38,12 +40,16 @@ namespace Jigsaw
                     if (mouseTargetDist < sideSize)
                     {
                         isSelected = true;
+                        isMouseDragging = true;
                     }
                     else isSelected = false;
                 }
             }
-            if (mState.LeftButton == ButtonState.Released) isSelected = false;
-
+            if (mState.LeftButton == ButtonState.Released) 
+            {
+                isMouseDragging = false;
+                isSelected = false;
+            } 
             if (isSelected)
             {
                 Vector2 mStateCenteredPosition = new Vector2(mState.Position.ToVector2().X - sideSize / 2, mState.Position.ToVector2().Y - sideSize / 2);
@@ -58,14 +64,11 @@ namespace Jigsaw
 
         private void setPosition(Vector2 newPosition)
         {
-            //rect.X = (int)(newPosition.X);
-            //rect.Y = (int)(newPosition.Y);
             position.X = (int)newPosition.X;
             position.Y = (int)newPosition.Y;
             centeredPosition.X = position.X + size.X / 2;
             centeredPosition.Y = position.Y + size.Y / 2;
         }
-
         public override void HandleCollision(GameObject gameObject)
         {
             if (gameObject.tag == "Slot" && !isSelected) setPosition(new Vector2(gameObject.rect.X, gameObject.rect.Y));
